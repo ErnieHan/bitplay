a8349181bce31afb80c691583ff5b749e918d521
Ernie <ernie.yeh.t.h@emphasis.com.tw>
2020-12-02 13:53:26 +0800

    update

--- pages/about.js
+++ pages/about.js
@@ -2,6 +2,7 @@ import Link from "next/link";
 export default () => (
   <div>
     <div>About us</div>
+    <div className="testing-image">this is testing</div>
     <h1 style={{ color: "blue" }}>THIS IS ABOUT PAGES</h1>
     <div>
       Back to
